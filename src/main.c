a23c7fa4eec893af17637ebb92cba342bd6e0293
Jacob Prebys <jacobprebys@gmail.com>
2023-02-22 20:36:20 -0600

    Add bitmap font rendering and write_str function

--- src/main.c
+++ src/main.c
@@ -16,15 +16,17 @@ int main()
     size_t width, height;
     p_frame *frame;
 
-    width = 600;
-    height = 400; 
+    width = 800;
+    height = 600; 
 
     if (pictoro_create_frame(&frame, width, height))
         error_and_die("create_frame");
     
     // Fill green and add purple rectangle
     pictoro_fill_frame(frame, 0x00FF00FF);
-    pictoro_fill_rect(frame, 100, 100, 500, 400, 0xFF00FFFF);
+    pictoro_fill_rect(frame, 150, 100, 500, 400, 0xFF00FFFF);
+
+    pictoro_write_str(frame, 200, 200, "Hello,\n  World!", 0xFFFFFFFF, 3);
 
     if (pictoro_save_frame(frame, "frame.ppm"))
         error_and_die("save_frame");
--- src/pictoro.h
+++ src/pictoro.h
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "constants.h"
+
 #define PPM_HEADER_MAXSIZE 256
 
 
@@ -66,6 +68,50 @@ void pictoro_fill_rect(p_frame *frame, size_t x, size_t y, size_t w, size_t h, u
                 frame->pixels[i * frame->width + j] = color;
             }
         }
+    }
+}
+
+
+void pictoro_write_str(p_frame *frame, const size_t x, const size_t y, 
+                          const char *str, const uint32_t color, const uint8_t font_size)
+{
+    const size_t column_spacing = font_size * 10;
+    const size_t row_spacing = font_size * 14;
+
+    size_t cursor_x = x;
+    size_t letter_x_start = x;
+    size_t cursor_y = y;
+    size_t letter_y_start = y;
+
+    for (size_t i = 0; i < strlen(str); ++i)
+    {
+        int char_code = str[i] - 32;
+        if (str[i] == '\n')
+        {
+            cursor_x = x;
+            letter_x_start = x;
+            cursor_y += row_spacing;
+            letter_y_start = cursor_y;
+            continue;
+        }
+
+        for (int j = 12; j >= 0; --j)
+        {
+            char scanline = bitmap_letters[char_code][j];
+            for (int k = 7; k >= 0; --k)
+            {
+                if (scanline & (1 << k))
+                {
+                    pictoro_fill_rect(frame, cursor_x, cursor_y, font_size, font_size, color);
+                }
+                cursor_x += font_size;
+            }
+            cursor_x = letter_x_start;
+            cursor_y += font_size;
+        }
+        cursor_y = letter_y_start;
+        cursor_x += column_spacing;
+        letter_x_start = cursor_x;
     }
 }
 
