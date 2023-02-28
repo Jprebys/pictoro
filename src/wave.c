ca24fcab2f605d00e1a7f0672e272cefa3000597
Jacob Prebys <jacobprebys@gmail.com>
2023-02-27 21:26:16 -0600

    Start work on rule establishment

--- src/wave.c
+++ src/wave.c
@@ -21,109 +21,160 @@ int get_cell_by_pos(CellGrid *grid, int x, int y)
     }
 }
 
-void print_subcell(SubCell *subcell, int subcell_size)
+void print_pattern(Pattern *pattern, int pattern_size)
 {
 
-    printf("\nPrinting subcell (count: %d)\n", subcell->count);
-    for (int i = 0; i < subcell_size; ++i)
+    printf("\nPrinting pattern (count: %d)\n", pattern->count);
+    for (int i = 0; i < pattern_size; ++i)
     {
-        for (int j = 0; j < subcell_size; ++j)
+        for (int j = 0; j < pattern_size; ++j)
         {
-            printf(" %d", subcell->values[i * subcell_size + j]);
+            printf(" %d", pattern->values[i * pattern_size + j]);
         }
         putchar('\n');
     }
 }
 
-bool subcells_equal(SubCell first, SubCell second, int subcell_size)
+
+bool patterns_equal(Pattern first, Pattern second, int pattern_size)
 {
     bool is_equal = true;
-    for (int i = 0; i < pow(subcell_size, 2); ++i)
+    for (int i = 0; i < pow(pattern_size, 2); ++i)
     {
         is_equal &= (first.values[i] == second.values[i]);
     }
     return is_equal;
 }
 
-SubCell *make_new_subcell(int subcell_size)
+
+Pattern *make_new_pattern(int pattern_size)
 {
-    SubCell *result = malloc(sizeof(SubCell));
-    result->values = malloc(pow(subcell_size, 2) * sizeof(int));
+    Pattern *result = malloc(sizeof(Pattern));
+    result->values = calloc(pow(pattern_size, 2), sizeof(int));
     result->count = 1;
     return result;
 }
 
 
-void copy_subcell(SubCell *dest, SubCell *src, int subcell_size)
+void copy_pattern(Pattern *dest, Pattern *src, int pattern_size)
 {
-    memcpy(dest->values, src->values, pow(subcell_size, 2) * sizeof(int));
+    memcpy(dest->values, src->values, pow(pattern_size, 2) * sizeof(int));
     dest->count = src->count;
 }
 
 
-int get_subcells(CellGrid *grid, int max_subcells, int subcell_size, SubCell **results)
+PatternList create_patternlist(int capacity)
+{
+    PatternList *result = malloc(sizeof(PatternList));
+    Pattern *patterns = calloc(capacity, sizeof(Pattern));
+
+    result->count = 0;
+    result->capacity = capacity;
+
+    return *result;
+}
+
+
+void patternlist_append(PatternList list, Pattern value)
+{
+    if (list.count > list.capacity / 2)
+    {
+        list.capacity *= 2;
+        list.patterns = realloc(list.patterns, list.capacity * sizeof(Pattern));
+    }
+    list.patterns[list.count++] = value;
+}
+
+
+
+int get_patterns(CellGrid *grid, int max_patterns, int pattern_size, Pattern **results)
 {
 
-    SubCell *subcells = malloc(sizeof(SubCell) * max_subcells);
+    Pattern *patterns = malloc(sizeof(Pattern) * max_patterns);
 
-    for (int i = 0; i < max_subcells; ++i)
+    for (int i = 0; i < max_patterns; ++i)
     {
-        subcells[i].values = malloc(pow(subcell_size, 2) * sizeof(int));
+        patterns[i].values = malloc(pow(pattern_size, 2) * sizeof(int));
     }
 
-    int subcell_count = 0;
-    for (int i = 0; i < grid->rows - (subcell_size - 1); ++i)
+    int pattern_count = 0;
+    for (int i = 0; i < grid->rows - (pattern_size - 1); ++i)
     {
-        for (int j = 0; j < grid->cols - (subcell_size - 1); ++j)
+        for (int j = 0; j < grid->cols - (pattern_size - 1); ++j)
         {
             int idx = 0;
-            SubCell *new = make_new_subcell(subcell_size);
+            Pattern *new = make_new_pattern(pattern_size);
 
 
-            for (int k = 0; k < subcell_size; ++k)
+            for (int k = 0; k < pattern_size; ++k)
             {
-                for (int l = 0; l < subcell_size; ++l)
+                for (int l = 0; l < pattern_size; ++l)
                 {
                     new->values[idx++] = get_cell_by_pos(grid, j + l, i + k);
                 }
             }
+
+            
             
             // check if already in list
             bool new_cell = true;
-            for (int k = 0; k < subcell_count; ++k)
+            for (int k = 0; k < pattern_count; ++k)
             {
 
-                if (subcells_equal(*new, subcells[k], subcell_size))
+                if (patterns_equal(*new, patterns[k], pattern_size))
                 {
-                    subcells[k].count++;
+                    patterns[k].count++;
                     new_cell = false;
                     break;
                 }
             }
             if (new_cell)
             {
-                copy_subcell(&subcells[subcell_count++], new, subcell_size);
+                copy_pattern(&patterns[pattern_count++], new, pattern_size);
             }
             free(new->values);
             free(new);
         }
     }
 
-    *results = subcells;
-    return subcell_count;
+    *results = patterns;
+    return pattern_count;
 }
 
 
-void run_wfc_algo(CellGrid *grid, int subcell_size)
+int get_pattern_idx(Pattern *patterns, int n_patterns, int *values, int n)
+{
+    for (int i = 0; i < n_patterns; ++i)
+    {
+        
+    }
+}
+
+
+void establish_rules(CellGrid *grid, Pattern *patterns, int n_patterns, int pattern_size, Rule *rules)
+{
+    for (int i = 0; i < grid->rows - (pattern_size - 1); ++i)
+    {
+        for (int j = 0; j < grid->cols - (pattern_size - 1); ++j)
+        {
+            int idx = 0;
+            int values[(int)pow(pattern_size, 2)];
+        }
+    }
+}
+
+
+void run_wfc_algo(CellGrid *grid, int pattern_size)
 {
 
-    SubCell *subcells;
-    int max_subcells = (grid->rows - (subcell_size - 1)) * (grid->cols - (subcell_size - 1));
+    Pattern *patterns;
+    int max_patterns = (grid->rows - (pattern_size - 1)) * (grid->cols - (pattern_size - 1));
 
 
-    int n_subcells = get_subcells(grid, max_subcells, subcell_size, &subcells);
+    int n_patterns = get_patterns(grid, max_patterns, pattern_size, &patterns);
 
-
+    Rule *rules = calloc(n_patterns, sizeof(Rule));
+    establish_rules(grid, patterns, n_patterns, pattern_size, rules);
 
 
 
@@ -131,17 +182,17 @@ void run_wfc_algo(CellGrid *grid, int subcell_size)
     
 
 
-    for (int i = 0; i < n_subcells; ++i)
+    for (int i = 0; i < n_patterns; ++i)
     {
-        print_subcell(&subcells[i], subcell_size);
+        print_pattern(&patterns[i], pattern_size);
     }
 
 
-    for (int i = 0; i < max_subcells; ++i)
+    for (int i = 0; i < max_patterns; ++i)
     {
-        free(subcells[i].values);;
+        free(patterns[i].values);;
     }
-    free(subcells);
+    free(patterns);
 
 }
 
--- src/wave.h
+++ src/wave.h
@@ -13,24 +13,37 @@ typedef struct CellGrid
 } CellGrid;
 
 
-typedef struct SubCell
+typedef struct Pattern
 {
     int *values;
     int count;
-} SubCell;
+} Pattern;
+
+
+// directions: upleft, up, upright, left, right, downleft, down, downright
+
+
+// For each pattern in our grid we need to have
+// a list of every potential pattern for each cardinal direction
+
+typedef struct PatternList
+{
+    Pattern *patterns;
+    int count, capacity;
+} PatternList;
+
+
+typedef struct Rule
+{
+    Pattern pattern;
+    PatternList adjacencies[8];
+} Rule;
 
 
 int get_cell_by_pos(CellGrid *grid, int x, int y);
 
-
-
-
-
-
-int get_subcells(CellGrid* grid, int max_subcells, int subcell_size, SubCell **results);
-
-
-void run_wfc_algo(CellGrid *grid, int subcell_size);
+int get_patterns(CellGrid* grid, int max_patterns, int pattern_size, Pattern **results);
+void run_wfc_algo(CellGrid *grid, int pattern_size);
 
 
 
